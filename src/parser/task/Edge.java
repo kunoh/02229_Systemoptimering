7d752822d229cb5ab7b4fb14c52d5539cab8faa5
Alexander Armstrong <s154302@student.dtu.dk>
2019-10-01 09:42:28 +0200

    Added Edge class

---
+++ src/parser/task/Edge.java
@@ -0,0 +1,32 @@
+package parser.task;
+
+public class Edge {
+
+    private String Source, Dest;
+
+    private int Cost;
+
+    public String getSource() {
+        return Source;
+    }
+
+    public void setSource(String source) {
+        Source = source;
+    }
+
+    public String getDest() {
+        return Dest;
+    }
+
+    public void setDest(String dest) {
+        Dest = dest;
+    }
+
+    public int getCost() {
+        return Cost;
+    }
+
+    public void setCost(int cost) {
+        Cost = cost;
+    }
+}
