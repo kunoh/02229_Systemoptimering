6508c4f94bc0cda6e756ba6129871d90256f908a
Alexander Armstrong <s154302@student.dtu.dk>
2019-10-01 09:37:07 +0200

    Added Task properties

--- src/parser/task/Task.java
+++ src/parser/task/Task.java
@@ -1,4 +1,40 @@
 package parser.task;
 
 public class Task {
+
+    public String Id, Name;
+
+    public int Wcet, Period, Deadline;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getWcet() {
+        return Wcet;
+    }
+
+    public void setWcet(int wcet) {
+        Wcet = wcet;
+    }
+
+    public int getPeriod() {
+        return Period;
+    }
+
+    public void setPeriod(int period) {
+        Period = period;
+    }
+
+    public int getDeadline() {
+        return Deadline;
+    }
+
+    public void setDeadline(int deadline) {
+        Deadline = deadline;
+    }
 }
