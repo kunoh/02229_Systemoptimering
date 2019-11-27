6082b974c5f1e68f627e4b4bcc97cb6a815202f5
Mathias Ennegaard Asmussen <s154219@win.dtu.dk>
2019-11-27 15:02:53 +0100

    Initial pritner commit

---
+++ CSharp/Optimization/Models/AssignedTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Optimization.Models
+{
+    class AssignedTask
+    {
+        public long Start { get; set; }
+        public long End { get; set; }
+        public string Cpu { get; set; }
+        public string Core { get; set; }
+        public int Index { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ CSharp/Optimization/SolutionPrinter.cs
@@ -0,0 +1,63 @@
+﻿using Google.OrTools.Sat;
+using Optimization.Models;
+using Optimization.Models.Architecture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Optimization
+{
+    class SolutionPrinter : CpSolverSolutionCallback
+    {
+        Dictionary<(int, int, int, int), TaskVar> taskVarDict;
+        List<Cpu> cpus;
+
+        public SolutionPrinter(Dictionary<(int, int, int, int), TaskVar> taskVarDict, List<Cpu> cpus)
+        {
+            this.taskVarDict = taskVarDict;
+            this.cpus = cpus;
+        }
+
+        public override void OnSolutionCallback()
+        {
+            string output = "";
+            var assignedTasks = new List<AssignedTask>();
+
+            foreach (var task in taskVarDict)
+            {
+                if (task.Value.IsActive == 1)
+                {
+                    assignedTasks.Add(new AssignedTask
+                    {
+                        Cpu = task.Key.Item1.ToString(),
+                        Core = task.Key.Item2.ToString(),
+                        Index = task.Key.Item3,
+                        Count = task.Key.Item4,
+                        Start = Value(task.Value.Start),
+                        End = Value(task.Value.End)
+                    });
+                }
+            }
+            
+            foreach (var cpu in cpus)
+            {
+                output += $"Cpu {cpu.Id}: \n";
+                foreach (var core in cpu.Cores)
+                {
+                    output += $"Core {core.Id}: ";
+
+                    foreach (var task in assignedTasks.Where(t => t.Cpu.Equals(cpu) && t.Core.Equals(core)).OrderBy(s => s.Start))
+                    {
+                        output += $"{task.Index}{task.Count}[{task.Start} - {task.End}] ";
+                    }
+
+                    output += "\n";
+                }
+            }
+
+        }
+        
+
+    }
+}
